f65802625d1bc48b6006b00856b14e135505da07
Gaurav Sharma <gaurav.sharma@zenways.io>
2019-01-24 12:09:30 +0530

    new

---
+++ HTML_CSS/js/main.js
@@ -0,0 +1,8 @@
+function myfunction() {
+    document.getElementById('demo').innerHTML="Hello World";
+}
+
+function myDate() {
+    var date = new Date();
+    document.getElementById('date').innerHTML=date.getFullYear();
+}
